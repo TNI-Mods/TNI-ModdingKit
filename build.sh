4f2c9cdd89b8a7da099c5a7b32d1ad17b2133869
MPrx_ <marc.proux+github@pm.me>
2026-02-18 23:42:51 +0100

    feat: build.sh update

--- build.sh
+++ build.sh
@@ -1,11 +1,25 @@
+#!/usr/bin/env bash
+
+LOCK_FILE=".lock"
+if [[ -f "${LOCK_FILE}" ]]; then exit 0 ; fi
+touch ${LOCK_FILE}
+
+ELF_DIR=".build/programs/luajitneo/CMakeFiles/luajit.elf.dir"
+if [[ -d "${ELF_DIR}" ]]; then 
+	find "${ELF_DIR}" -name "*.o" -delete
+fi
+
+CORES=$(( $( grep -c 'processor' /proc/cpuinfo ) - 1 ))
+
 export CC="riscv64-linux-gnu-gcc-14"
 export CXX="riscv64-linux-gnu-g++-14"
 
 mkdir -p .build
-pushd .build
-cmake .. -G Ninja -DCMAKE_COLOR_DIAGNOSTICS=ON -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_BUILD_TYPE=RelWithDebInfo -DSTRIPPED=OFF
-cmake --build . -j
+pushd .build 1>/dev/null || exit
+cmake .. -G Ninja -DCMAKE_COLOR_DIAGNOSTICS=ON -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_BUILD_TYPE=RelWithDebInfo -DSTRIPPED=OFF 
+cmake --build . -j "${CORES}"
 BUILD_EXIT_CODE=$?
-popd
+popd 1>/dev/null || exit
 
+rm -f ${LOCK_FILE}
 exit $BUILD_EXIT_CODE
