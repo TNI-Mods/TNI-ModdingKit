8512d11daa593da45c2344dba10aa3dc316e0650
MarcProux <marc.proux+github@pm.me>
2026-03-05 00:39:39 +0100

    fix: improved ELF_DIR and add CLASS clean

--- build.sh
+++ build.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
+CLASS="${1:-*}"
+
 LOCK_FILE=".lock"
 if [[ -f "${LOCK_FILE}" ]]; then exit 0 ; fi
 touch ${LOCK_FILE}
-
-ELF_DIR=".build/programs/luajitneo/CMakeFiles/luajit.elf.dir"
-if [[ -d "${ELF_DIR}" ]]; then 
-	find "${ELF_DIR}" -name "*.o" -delete
-fi
 
 CORES=$(( $( grep -c 'processor' /proc/cpuinfo ) - 1 ))
 
@@ -15,6 +12,12 @@ export CC="riscv64-linux-gnu-gcc-14"
 export CXX="riscv64-linux-gnu-g++-14"
 
 mkdir -p .build
+
+ELF_DIR="$( find ".build" -type d -name 'luajit.elf.dir' | head -n 1 )"
+if [[ -d "${ELF_DIR}" ]]; then
+	find "${ELF_DIR}" -name "${CLASS}.*.o" -delete
+fi
+
 pushd .build 1>/dev/null || exit
 cmake .. -G Ninja -DCMAKE_COLOR_DIAGNOSTICS=ON -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_BUILD_TYPE=RelWithDebInfo -DSTRIPPED=OFF 
 cmake --build . -j "${CORES}"
